6fb94ed39b1538634c774a297ae64c236f3b3fba
Thwipshot <ethumpert@gmail.com>
2016-07-19 16:40:42 -0500

    made progress in one of the views functionality and moved it to the applicable view file

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,6 @@ if (window.comicSearch === undefined) { window.comicSearch = {}; }
 (function(context) {
 
 
-        var codeFriendlyinput = encodeURIComponent("Doctor Strange");
-
-        var result = $.ajax({
-            url: "http://gateway.marvel.com:80/v1/public/characters?name=" + codeFriendlyinput + "&apikey=1103dc5941f198d7daedd7998113e339",
-        });
-
-        result.done(function(comics) {
-          console.log("query result:", comics );
-        })
-
 
   var dataModel = new context.DataModel();
 
