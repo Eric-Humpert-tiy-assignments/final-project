3e48753e566cdfca3e83fb183e30152f3245d796
Thwipshot <ethumpert@gmail.com>
2016-07-19 15:50:51 -0500

    started work in the app.js file and will start creating more files for the app as it grows

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,18 @@ if (window.comicSearch === undefined) { window.comicSearch = {}; }
 
 (function(context) {
 
+
+        var codeFriendlyinput = encodeURIComponent("Doctor Strange");
+
+        var result = $.ajax({
+            url: "http://gateway.marvel.com:80/v1/public/characters?name=" + codeFriendlyinput + "&apikey=1103dc5941f198d7daedd7998113e339",
+        });
+
+        result.done(function(comics) {
+          console.log("query result:", comics );
+        })
+
+
   var dataModel = new context.DataModel();
 
   var mainView = new context.MainView({ el: $("#view-container")});
--- public/js/data-grabber.js
+++ public/js/data-grabber.js
@@ -2,34 +2,10 @@ if (window.comicSearch === undefined) { window.comicSearch = {}; }
 
 (function(context) {
 
-    var DataGrabber = Backbone.View.extend({
+  var DataGrabber = Backbone.View.extend({
 
-    events: {
-      "keyup #query-box": "heroQuery",
-    },
+  });
 
-    heroQuery: function(evt) {
+  context.DataGrabber = DataGrabber;
 
-      var query = this;
-
-      if (evt.keycode === 13) {
-        var input = query.$("#query-box").val();
-
-        console.log(input);
-
-      }
-    /*
-        console.log(input);
-        //codeFriendlyinput = encodeURIComponent(input);
-
-        $.ajax({
-            url: "http://gateway.marvel.com:80/v1/public/characters?" + codeFriendlyinput + "&apikey=1103dc5941f198d7daedd7998113e339",
-        })
-
-        console.log("http://gateway.marvel.com:80/v1/public/characters?" + codeFriendlyinput + "&apikey=1103dc5941f198d7daedd7998113e339");
-    */
-    }
-
-    });
-    context.DataGrabber = DataGrabber;
 })(window.comicSearch);
