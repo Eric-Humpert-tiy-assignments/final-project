55052e1706a54291ff5d66f08d134c4c806196e0
Thwipshot <ethumpert@gmail.com>
2016-07-29 15:38:02 -0500

    created a view for managing and rendering the character image returned when a user searches for comics

---
+++ public/js/character-image.js
@@ -0,0 +1,10 @@
+if (window.comicSearch === undefined) { window.comicSearch = {}; }
+
+(function(context) {
+
+  var CharacterImage = Backbone.View.extend({
+
+  });
+
+  context.CharacterImage = CharacterImage;
+})(window.comicSearch);
