2da8bd3700479cb2b8996d38d193d89af84261df
Thwipshot <ethumpert@gmail.com>
2016-12-10 14:46:13 -0600

    added babel es6 translating functionality to the code and started to implement a collectionView for rendering all the models in the collection

--- public/js/views/character/favorite-characters-view.js
+++ public/js/views/character/favorite-characters-view.js
@@ -7,7 +7,41 @@ if (window.comicSearch === undefined) { window.comicSearch = {}; }
     events: {
       "click .search-return": "goToSearch",
     },
-    
+
+    initialize() {
+      this.listenTo(this.collection, 'add', this.addModel);
+    },
+
+    modelAdded(model) {
+      var view = this.renderModel(model);
+      this.$el.append(view.$el);
+    },
+
+    render() {
+      var html = this.collection.map(model => {
+        var view = this.renderModel(model);
+        view.render();
+        return view.$el;
+      })
+
+      this.$el.html(html);
+      return this;
+    },
+
+    renderModel (model) {
+      var view = new this.characterView({ model: model});
+
+      //view model relation tracker
+      this.children[model.cid] = view;
+
+      this.listenTo(view, 'all', eventName => {
+        this.trigger('item:' + eventName, view, model);
+      });
+
+      view.render();
+      return view;
+    },
+
 
     goToSearch: function() {
       console.log("You clicked search return");
